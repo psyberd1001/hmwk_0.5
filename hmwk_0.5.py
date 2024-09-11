7d835026c27d874826b83761851d5eeaae3cc47d
psyberd1001 <108468415+psyberd1001@users.noreply.github.com>
2024-09-11 11:35:56 +0300

    Add files via upload

---
+++ hmwk_0.5.py
@@ -0,0 +1,13 @@
+def  get_matrix(n, m, value):
+    matrix = []
+    matrix_1 = []
+    for i in range(n):
+        matrix.append(matrix_1)
+    for i in range(m):
+        matrix_1.append(value)
+
+    return matrix
+
+print(get_matrix(2, 2, 10))
+print(get_matrix(3, 5, 42))
+print(get_matrix(4, 2, 13))
